cefa9f9aaa870ebe7bad4d56c4751b29b0c97fa0
uditjuneja1 <uditjuneja1@gmail.com>
2018-06-12 19:08:23 +0530

    commit

--- flask_app/fourms.py
+++ flask_app/fourms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_app.models import User
@@ -26,3 +28,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('Username already exist!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first()
+            if email:
+                raise ValidationError('E-mail already exist!')
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,8 +1,16 @@
+import os
 from flask import render_template, url_for, flash, redirect, request
 from flask_app import app, db, bcrypt
-from flask_app.fourms import RegistrationForm, LoginForm
+from flask_app.fourms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+
+def token_hex(length=8, charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"):
+    random_bytes = os.urandom(length)
+    len_charset = len(charset)
+    indices = [int(len_charset * (ord(byte) / 256.0)) for byte in random_bytes]
+    return "".join([charset[index] for index in indices])
+
 
 posts = [
     {
@@ -60,7 +68,33 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    if current_user.image_file != 'static/profile_photos/' + 'default.jpeg':
+        prev_photo = os.path.join(app.root_path, 'static/profile_photos', current_user.image_file)
+        os.remove(prev_photo)
+    random_hex = token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_photos', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods = ['GET','POST'])
 @login_required
 def account():
-    return render_template("account.html", title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if current_user.image_file:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_photos/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
